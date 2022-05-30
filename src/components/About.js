e05327acbb3de329631eaba076969e6796bc9aec
Hutomo Sugianto <hutomosugianto@gmail.com>
2022-05-30 14:07:41 +0700

    Update component link

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>About Page</h1>
 
       <div className="play-container">
-        <p>Source code can be seen on ...</p>
+        <p>Source code can be seen on <a href="https://github.com/hutomo-s/pokedex">https://github.com/hutomo-s/pokedex</a></p>
       </div>
     </>
   )
